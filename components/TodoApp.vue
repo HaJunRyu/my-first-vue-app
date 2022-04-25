abb8a4c69b8e65a115935b9aaea4e61ba374c73d
HaJunRyu <hjr4557@naver.com>
2022-04-25 15:34:00 +0900

    Todo 기본 CRUD 완성

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <todo-item />
+    <todo-item
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @update-todo="updateTodo"
+      @delete-todo="deleteTodo"
+      @edited-todo="editedTodo"
+    />
+    <hr />
     <todo-creator @create-todo="createTodo" />
   </div>
 </template>
@@ -8,6 +16,9 @@
 <script>
 import lowdb from 'lowdb';
 import LocalStorage from 'lowdb/adapters/LocalStorage';
+import cloneDeep from 'lodash/cloneDeep';
+import find from 'lodash/find';
+import assign from 'lodash/assign';
 import TodoCreator from './TodoCreator';
 import TodoItem from './TodoItem';
 
@@ -19,6 +30,7 @@ export default {
   data() {
     return {
       db: null,
+      todos: [],
     };
   },
   created() {
@@ -29,11 +41,17 @@ export default {
       const adapter = new LocalStorage('todo-app');
       this.db = lowdb(adapter);
 
-      this.db
-        .defaults({
-          todos: [],
-        })
-        .write();
+      const hasTodos = this.db.has('todos').value();
+
+      if (hasTodos) {
+        this.todos = cloneDeep(this.db.getState().todos);
+      } else {
+        this.db
+          .defaults({
+            todos: [],
+          })
+          .write();
+      }
     },
     createTodo(title) {
       const currentDate = new Date();
@@ -50,6 +68,24 @@ export default {
       };
 
       this.db.get('todos').push(newTodo).write();
+
+      this.todos.push(newTodo);
+    },
+    updateTodo(todo, updateValue) {
+      this.db.get('todos').find({ id: todo.id }).assign(updateValue).write();
+
+      const foundTodo = find(this.todos, { id: todo.id });
+      assign(foundTodo, updateValue);
+    },
+    deleteTodo(todo) {
+      this.db.get('todos').remove({ id: todo.id }).write();
+
+      const foundIndex = this.todos.findIndex(({ id }) => id === todo.id);
+
+      this.$delete(this.todos, foundIndex);
+    },
+    editedTodo(todo, title) {
+      console.log('editedTodo!', todo, title);
     },
   },
 };
--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,3 +1,115 @@
 <template>
-  <h2>Todo Item</h2>
+  <div :class="{ done: done }" class="todo-item">
+    <div v-if="isEditMode" class="item__inner item--edit">
+      <input
+        ref="titleInput"
+        type="text"
+        :value="title"
+        @input="title = $event.target.value"
+        @keypress.enter="editedTodo"
+        @keypress.esc="offEditMode"
+      />
+      <div class="item__actions">
+        <button key="complete" @click="editedTodo">완료</button>
+        <button key="cancel" @click="offEditMode">취소</button>
+      </div>
+    </div>
+    <div v-else class="item__inner item--normal">
+      <input v-model="done" type="checkbox" />
+      <div class="item__title-wrap">
+        <div class="item__title">
+          {{ todo.title }}
+        </div>
+        <div class="item__date">
+          {{ date }}
+        </div>
+      </div>
+      <div class="item__actions">
+        <button key="update" @click="onEditMode">수정</button>
+        <button key="delete" @click="deleteTodo">삭제</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import dayjs from 'dayjs';
+
+export default {
+  props: {
+    todo: Object,
+  },
+  data() {
+    return {
+      isEditMode: false,
+      title: this.todo.title,
+    };
+  },
+  computed: {
+    done: {
+      get() {
+        return this.todo.done;
+      },
+      set(done) {
+        this.updateTodo({ done });
+      },
+    },
+    date() {
+      const date = dayjs(this.todo.createdAt);
+      const isSame = date.isSame(this.todo.updatedAt);
+
+      if (isSame) {
+        return date.format('YYYY년 MM월 DD일');
+      } else {
+        return `${date.format('YYYY년 MM월 DD일')} (edited)`;
+      }
+    },
+  },
+  methods: {
+    onEditMode() {
+      this.isEditMode = true;
+
+      this.$nextTick(() => {
+        this.$refs.titleInput.focus();
+      });
+    },
+    offEditMode() {
+      this.isEditMode = false;
+    },
+    editedTodo() {
+      if (this.title === this.todo.title) return;
+
+      this.updateTodo({
+        title: this.title,
+        updatedAt: new Date(),
+      });
+
+      this.offEditMode();
+    },
+    updateTodo(value) {
+      this.$emit('update-todo', this.todo, value);
+    },
+    deleteTodo() {
+      this.$emit('delete-todo', this.todo);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-item {
+  margin-bottom: 10px;
+  .item__inner {
+    display: flex;
+  }
+  .item__date {
+    font-size: 12px;
+  }
+
+  &.done {
+    .item__title {
+      text-decoration: line-through;
+    }
+  }
+}
+</style>
